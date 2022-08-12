5b0c78d65f018846223570344b8527d4d56deaca
lucin <theeugenet@gmail.com>
2022-08-12 14:12:56 +0700

    Fucked jitpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,20 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.10"
+    `maven-publish`
     application
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "io.lucin"
+            artifactId = "library"
+            version = "1.0.2"
+
+            from(components["java"])
+        }
+    }
 }
 
 tasks.withType<KotlinCompile> {
@@ -19,17 +32,18 @@ dependencies {
 
     compileOnly("com.github.Anuken.Arc:arc-core:$mindustryVersion")
     compileOnly("com.github.Anuken.Mindustry:core:$mindustryVersion")
-    // compileOnly("com.github.Anuken.Mindustry:server:$mindustryVersion")
 
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.7.10")
 }
 
 tasks.jar {
-    archiveFileName.set("${project.name}.jar")
+    archiveFileName.set("${project.name}-${project.version}.jar")
+
     from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) }) {
         exclude("**/META-INF/*.SF")
         exclude("**/META-INF/*.DSA")
         exclude("**/META-INF/*.RSA")
     }
+
     duplicatesStrategy = DuplicatesStrategy.INCLUDE
 }
